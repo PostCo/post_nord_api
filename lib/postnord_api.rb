d16646b7034b942b0a9305aa805cd3e25928f99c
Linh <hplinh0702@gmail.com>
2025-09-25 11:34:10 +0700

    Initial commit

---
+++ lib/postnord_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'postnord_api/version'
+
+module PostnordAPI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/postnord_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PostnordAPI
+  VERSION = '0.1.0'
+end
