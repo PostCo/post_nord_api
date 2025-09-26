b95c74ea2f85674c17a20600818782ac5b8ceccf
Linh <hplinh0702@gmail.com>
2025-09-26 14:00:24 +0700

    Parse shipment create return label params from snake_case to camelCase

--- lib/post_nord_api/resources/shipment_resource.rb
+++ lib/post_nord_api/resources/shipment_resource.rb
@@ -3,11 +3,11 @@
 module PostNordAPI
   class ShipmentResource < BaseResource
     def create_return_label(params)
-      response = post_request("shipment/v3/returns/edi/labels/zpl", body: params)
+      response = post_request('shipment/v3/returns/edi/labels/zpl', body: parse_params(params))
       Booking.new(response.body)
     end
 
-    def track(tracking_number, locale: "en")
+    def track(tracking_number, locale: 'en')
       response = get_request("shipment/v5/trackandtrace/findByIdentifier.json?id=#{tracking_number}&locale=#{locale}")
       Tracking.new(response.body)
     end
